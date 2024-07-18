d50587f9de74d753c316b0b76fa48d1046ff643d
Mk281005 <manoj281005@gmail.com>
2024-07-18 23:28:55 +0530

    Update it

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
 function Sidebar() {
   return (
-    <div>
-        SideBar
+    <div className='bg-green-400 h-9 flex  items-center gap-8 p-4 justify-between'>
+      <div className='font-bold text-xl font-sans '>NearMe</div>
+      <div className='flex justify-evenly gap-16 mr-16'>
+      <img className='w-6 ' src="home.png" alt="" />
+      <img className='w-6 ' src="heart.png" alt="" /></div>
     </div>
   )
 }
